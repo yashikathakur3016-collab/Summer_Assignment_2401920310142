9c354a388acd0e12ef56b682641e321b42020fe8
yashikathakur3016-collab <yashikathakur3016@gmail.com>
2026-06-18 22:35:44 +0530

    Create question3.cpp

---
+++ week3/day4/question3.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        int n = heights.size();
+        int maxArea = 0;
+
+        for (int i = 0; i <= n; i++) {
+            int currHeight = (i == n) ? 0 : heights[i];
+
+            while (!st.empty() && heights[st.top()] > currHeight) {
+                int height = heights[st.top()];
+                st.pop();
+
+                int width;
+                if (st.empty())
+                    width = i;
+                else
+                    width = i - st.top() - 1;
+
+                maxArea = max(maxArea, height * width);
+            }
+
+            st.push(i);
+        }
+
+        return maxArea;
+    }
+};
