7761a095508766200c22abb7f81c83d40ee06ef6
yashikathakur3016-collab <yashikathakur3016@gmail.com>
2026-06-19 22:19:35 +0530

    Implement RecentCounter class with ping method

---
+++ week3/day5/question2.cpp
@@ -0,0 +1,17 @@
+#include <queue>
+using namespace std;
+class RecentCounter {
+private:
+    queue<int> q;
+public:
+    RecentCounter() {
+        
+    }
+    int ping(int t) {
+        q.push(t);
+        while (q.front() < t - 3000) {
+            q.pop();
+        }
+        return q.size();
+    }
+};
