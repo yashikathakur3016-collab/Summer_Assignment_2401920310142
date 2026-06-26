132bada3b53d1bef85bd567e605eb6d139e2a6e1
yashikathakur3016-collab <yashikathakur3016@gmail.com>
2026-06-26 22:25:47 +0530

    Create q1.cpp

---
+++ week4/day5/q1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (!root) return true;
+        return isMirror(root->left, root->right);
+    }
+    
+private:
+    bool isMirror(TreeNode* left, TreeNode* right) {
+        if (!left && !right) return true;
+        if (!left || !right) return false;
+        
+        return (left->val == right->val)
+            && isMirror(left->left, right->right)
+            && isMirror(left->right, right->left);
+    }
+};
